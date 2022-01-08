df58f5402f580de0d35dd39a7e4f5140cb93bbd0
Henry Wang <henrywang97@gmail.com>
2022-01-08 15:22:18 +0000

    add dactyl manufom

---
+++ dactyl_manuform/mad_beakl_scientist/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define USE_SERIAL
+
+#define MASTER_LEFT
+//#define MASTER_RIGHT
+
+//#define EE_HANDS
+
+#undef DEBOUNCE
+#define DEBOUNCE 5
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 10 
+
+#define RETRO_TAPPING
