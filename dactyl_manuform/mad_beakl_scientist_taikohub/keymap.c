042b53714130e64995217f64f34b58b94a7bcedf
Henry Wang <henrywang97@gmail.com>
2022-12-03 21:49:31 +0000

    update keymap

--- dactyl_manuform/mad_beakl_scientist_taikohub/keymap.c
+++ dactyl_manuform/mad_beakl_scientist_taikohub/keymap.c
@@ -105,58 +105,57 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_5x6(
-        LGUI(KC_F), WRITE_VIM, KC_LCBR, KC_LPRN, KC_LBRACKET, QUIT_VIM,                         KC_TRANSPARENT, KC_RBRACKET, KC_RPRN, KC_RCBR, KC_COLON, KC_DELETE,
-        KC_TAB, KC_Q, KC_Y, KC_O, KC_U, KC_Z,                                                              KC_W, KC_D, KC_N, KC_C, KC_B, KC_F12,
-        LSFT_T(KC_BSPC), KC_H, KC_I, KC_E, KC_A, KC_UNDS,                                                KC_G, KC_T, KC_R, KC_S, KC_P, LSFT_T(KC_EQUAL),
-        KC_LCTRL, LT(1,KC_QUOTE), LT(1,KC_COMMA), KC_J, KC_K, KC_X,                                        KC_V, KC_M, KC_L, KC_F, LT(4,KC_SLASH), RCTL_T(KC_SCOLON),
-                  KC_LGUI, KC_LALT,                                                                                    KC_LEFT, KC_RIGHT,
-                                             LT(2,KC_ESCAPE), LT(3,KC_DOT),                        KC_SPACE, LT(4,KC_ENTER),
-                                                     KC_LALT, KC_LGUI,                                    KC_END,  KC_DEL,
-                                                     KC_HOME, KC_GRV,                                   KC_LGUI, KC_LALT
+        LGUI(KC_F), WRITE_VIM, KC_LCBR, KC_LPRN, KC_LBRACKET, QUIT_VIM,                _______, KC_RBRACKET, KC_RPRN, KC_RCBR, KC_COLON, KC_DELETE,
+        KC_TAB, KC_Q, KC_Y, KC_O, KC_U, KC_Z,                                          KC_W, KC_D, KC_N, KC_C, KC_B, KC_F12,
+        LSFT_T(KC_BSPC), KC_H, KC_I, KC_E, KC_A, KC_UNDS,                              KC_G, KC_T, KC_R, KC_S, KC_P, LSFT_T(KC_EQUAL),
+        KC_LCTRL, LT(2,KC_QUOTE), LT(2,KC_COMMA), KC_J, KC_K, KC_X,                    KC_V, KC_M, KC_L, KC_F, LT(4,KC_SLASH), RCTL_T(KC_SCOLON),
+                  KC_LGUI, KC_LALT,                                                                           KC_LEFT, KC_RIGHT,
+                                             LT(1,KC_ESCAPE), LT(3,KC_DOT),               KC_SPACE, LT(4,KC_ENTER),
+                                                     KC_LGUI, KC_LALT,                           KC_END,  KC_DEL,
+                                                     KC_HOME, KC_GRV,                          KC_LGUI, KC_LALT
     ),
 
     [TILING_WM] = LAYOUT_5x6(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, LGUI(LSFT(KC_F1)), LGUI(LSFT(KC_F2)), LGUI(LSFT(KC_F3)), LGUI(LSFT(KC_F4)), LGUI(LSFT(KC_F5)),
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 LGUI(KC_W), LGUI(KC_D), LGUI(KC_UP), LGUI(KC_C), LGUI(KC_J), LGUI(LSFT(KC_F6)),
-    KC_TRANSPARENT, KC_TRANSPARENT, LGUI(KC_I),     LGUI(KC_E),     LGUI(KC_A),     KC_TRANSPARENT,                                 LGUI(KC_G), LGUI(KC_LEFT), LGUI(KC_DOWN),  LGUI(KC_RIGHT), LGUI(KC_P),     LGUI(LSFT(KC_F7)),
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, RCTL(KC_J),     RCTL(KC_K),     LGUI(KC_X),                                     LGUI(KC_V),     LGUI(KC_1),     LGUI(KC_2),     LGUI(KC_3),     LGUI(KC_4),     LGUI(KC_5),
-                    KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 LGUI(KC_F1),    LGUI(KC_F2),
-                                                                 KC_TRANSPARENT, KC_LSHIFT,                                                          LGUI(KC_SPACE), LGUI(KC_ENTER),
-                                                                     KC_TRANSPARENT,KC_TRANSPARENT,                                    KC_TRANSPARENT,  KC_TRANSPARENT,
-                                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                   KC_TRANSPARENT, KC_TRANSPARENT
+    LALT(LSFT(KC_TAB)), _______, _______, _______, _______, _______,                    _______, LGUI(LSFT(KC_F1)), LGUI(LSFT(KC_F2)), LGUI(LSFT(KC_F3)), LGUI(LSFT(KC_F4)), LGUI(LSFT(KC_F5)),
+    LALT(KC_TAB), QUIT_VIM, _______, _______, _______, _______,                         LGUI(KC_W), LGUI(KC_D), LGUI(KC_UP), LGUI(KC_C), LGUI(KC_J), LGUI(LSFT(KC_F6)),
+    LCTL(KC_BSPACE), LCTL(KC_W), WRITE_VIM, KC_LSHIFT, LGUI(KC_A), _______,             LGUI(KC_G), LGUI(KC_LEFT), LGUI(KC_DOWN), LGUI(KC_RIGHT), LGUI(KC_P), LGUI(LSFT(KC_F7)),
+    LCTL(LSFT(KC_Z)), KC_PC_UNDO, KC_PC_CUT, KC_PC_COPY, KC_PC_PASTE, LGUI(KC_X),       LGUI(KC_V), LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),
+                    _______, _______,                                                                                         LGUI(KC_F1), LGUI(KC_F1),
+                                                 _______, _______,                              LGUI(KC_SPACE), LGUI(KC_ENTER),
+                                                     _______,_______,                      _______,  _______,
+                                                     _______, _______,                     _______, _______
     ),
 
     [SHORTCUTS] = LAYOUT_5x6(
-      LALT(LSFT(KC_TAB)), KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-      LALT(KC_TAB),   QUIT_VIM,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                  KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-      LCTL(KC_BSPACE),LCTL(KC_W),     WRITE_VIM,     KC_TRANSPARENT, LCTL(KC_A),     KC_TRANSPARENT,                                                                 KC_TRANSPARENT, HARPOON_FILE_1, HARPOON_FILE_2, HARPOON_FILE_3, HARPOON_FILE_4, HARPOON_FILE_5,
-      LCTL(LSFT(KC_Z)),KC_PC_UNDO,     KC_PC_CUT,      KC_PC_COPY,     KC_PC_PASTE,    KC_TRANSPARENT,                                                           KC_TRANSPARENT, HARPOON_ADD_FILE, HARPOON_QUICK_MENU, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-                                   KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                                          KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                         KC_TRANSPARENT, KC_TRANSPARENT,                                                          KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                             KC_TRANSPARENT,KC_TRANSPARENT,                                    KC_TRANSPARENT,  KC_TRANSPARENT,
-                                                                             KC_TRANSPARENT, KC_TRANSPARENT,                                   KC_TRANSPARENT, KC_TRANSPARENT
-            ),
-
-    [SYMBOLS] = LAYOUT_5x6(
-      RESET, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_TILD,        KC_AT,          KC_PERC,        KC_EXLM,        KC_GRAVE,                                KC_AMPR,        KC_7,           KC_8,           KC_9,           KC_BSLASH,      KC_PIPE,
-      KC_TRANSPARENT,KC_PLUS,        KC_CIRC,        KC_COLN,        KC_DLR,         KC_HASH,                                  KC_ASTR,        KC_4,           KC_5,           KC_6,           KC_MINUS,       RIGHT_ARROW,
-      KC_TRANSPARENT, KC_DQUO,        KC_COMMA,       KC_J,           KC_K,           KC_LABK,                                 KC_RABK,        KC_1,           KC_2,           KC_3,           KC_QUES,        KC_DOT,
-                                      KC_TRANSPARENT, KC_TRANSPARENT,                                                                                          KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                       KC_TRANSPARENT, KC_TRANSPARENT,                                                    KC_TRANSPARENT, KC_0,
-                                                                             KC_TRANSPARENT,KC_TRANSPARENT,                                    KC_TRANSPARENT,  KC_TRANSPARENT,
-                                                                             KC_TRANSPARENT, KC_TRANSPARENT,                                   KC_TRANSPARENT, KC_TRANSPARENT
+      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+      _______, _______,     _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______,
+      _______, _______,  _______, _______, _______, _______,                           _______, HARPOON_FILE_1, HARPOON_FILE_2, HARPOON_FILE_3, HARPOON_FILE_4, HARPOON_FILE_5,
+      _______, _______, _______, RCTL(KC_J), RCTL(KC_K), _______,                      _______, HARPOON_ADD_FILE, HARPOON_QUICK_MENU, _______, _______, _______,
+                                   _______, _______,                                                    _______, _______,
+                                                 _______, _______,                            _______, _______,
+                                                     _______,_______,                      _______,  _______,
+                                                     _______, _______,                     _______, _______
     ),
 
+    [SYMBOLS] = LAYOUT_5x6(
+      RESET, _______, _______, _______, _______, _______,                              _______, _______, _______, _______, _______, _______,
+      _______, KC_TILD, KC_AT, KC_PERC, KC_EXLM, KC_GRAVE,                             KC_AMPR, KC_7, KC_8, KC_9, KC_BSLASH, KC_PIPE,
+      _______, KC_PLUS, KC_CIRC, KC_COLN, KC_DLR, KC_HASH,                              KC_ASTR, KC_4, KC_5, KC_6, KC_MINUS, RIGHT_ARROW,
+      _______, KC_DQUO, KC_COMMA, KC_J, KC_K, KC_LABK,                                 KC_RABK, KC_1, KC_2, KC_3, KC_QUES,  KC_DOT,
+                                      _______, _______,                                                  _______, _______,
+                                               _______, _______,                            _______, KC_0,
+                                                     _______,_______,                  _______,  _______,
+                                                     _______, _______,                 _______, _______
+    ),
     [FUNCTIONS] = LAYOUT_5x6(
-      KC_TRANSPARENT, LSFT(KC_F4),    LSFT(KC_F5),    LSFT(KC_F6),    LSFT(KC_F7),    LSFT(KC_F8),                                               KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_F12,         KC_F7,          KC_F8,          KC_F9,          LSFT(KC_F3),                                            KC_TRANSPARENT, KC_HOME,        KC_UP,          KC_END,         KC_TRANSPARENT, LCTL(KC_F10),
-      KC_TRANSPARENT, KC_F11,         KC_F4,          KC_F5,          KC_F6,          LSFT(KC_F2),                                                                    KC_TRANSPARENT, KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_F10,         KC_F1,          KC_F2,          KC_F3,          LSFT(KC_F1),                                                KC_TRANSPARENT, LCTL(KC_LEFT),  KC_CAPSLOCK,    RCTL(KC_RIGHT), KC_TRANSPARENT, KC_TRANSPARENT,
-                                      KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT,                                                
-                                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                                          KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                             KC_TRANSPARENT,KC_TRANSPARENT,                                    KC_TRANSPARENT,  KC_TRANSPARENT,
-                                                                             KC_TRANSPARENT, KC_TRANSPARENT,                                   KC_TRANSPARENT, KC_TRANSPARENT
+      _______, LSFT(KC_F4), LSFT(KC_F5), LSFT(KC_F6), LSFT(KC_F7), LSFT(KC_F8),     _______, _______, _______, _______, _______, _______,
+      _______, KC_F12, KC_F7, KC_F8, KC_F9, LSFT(KC_F3),                                 _______, KC_HOME, KC_UP, KC_END, _______, LCTL(KC_F10),
+      _______, KC_F11, KC_F4, KC_F5, KC_F6, LSFT(KC_F2),                                 _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,
+      _______, KC_F10, KC_F1, KC_F2, KC_F3, LSFT(KC_F1),                                 _______, LCTL(KC_LEFT), KC_CAPSLOCK, RCTL(KC_RIGHT), _______, _______,
+                                      _______, _______,                                                _______, _______,                                                
+                                                 _______, _______,                       _______, _______,
+                                                     _______,_______,                _______,  _______,
+                                                     _______, _______,               _______, _______
     ),
 }  ;
 
